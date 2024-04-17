cbc56c2d2c3d5bff2422d468d7558f692e2f1847
kenielf <chrystian.keni.github@gmail.com>
2024-04-17 20:50:34 -0300

    Testing look and feel configuration.

--- src/main/java/com/dreaminhouse/Main.java
+++ src/main/java/com/dreaminhouse/Main.java
@@ -1,9 +1,13 @@
 package com.dreaminhouse;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import com.dreaminhouse.controllers.UserController;
 import com.dreaminhouse.models.User;
 import com.dreaminhouse.platform.OperatingSystem;
 import com.dreaminhouse.views.MainWindow;
+import com.formdev.flatlaf.intellijthemes.FlatLightFlatIJTheme;
 
 /**
  * Main
@@ -20,6 +24,13 @@ public class Main {
         UserController uController = new UserController();
         // uController.createUser("Claudio");
         // uController.createUser("Marcelo");
+
+        // Install Theme
+        try {
+            UIManager.setLookAndFeel(new FlatLightFlatIJTheme());
+        } catch (UnsupportedLookAndFeelException exception) {
+            System.out.println("Failed to apply look and feel: " + exception);
+        }
 
         // for (User user : UserController.getAllUsers()) {
         //     System.out.format("User: %s\n", user.getUserName());
